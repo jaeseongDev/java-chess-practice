94418f94f813d7dcb719b338b40e3c58594a636d
jaeseongDev <qkrwotjd1445@naver.com>
2021-03-15 17:25:49 +0900

    test : PieceStatus에 대한 테스트 코드 추가

--- src/main/java/chess/domain/piece/PieceStatus.java
+++ src/main/java/chess/domain/piece/PieceStatus.java
@@ -1,10 +1,10 @@
 package chess.domain.piece;
 
 public class PieceStatus {
-    private final String position;
+    private final PiecePosition position;
     private final Piece piece;
 
-    public PieceStatus(String position, Piece piece) {
+    public PieceStatus(PiecePosition position, Piece piece) {
         this.position = position;
         this.piece = piece;
     }
---
+++ src/test/java/chess/domain/piece/PieceStatusTest.java
@@ -0,0 +1,18 @@
+package chess.domain.piece;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+public class PieceStatusTest {
+    @Test
+    @DisplayName("체스의 포지션을 정상적으로 생성하는 지 테스트")
+    public void init() {
+        assertThatCode(() -> {
+            new PiecePosition('a', '2');
+            Piece piece = new Piece(PieceType.BISHOP, PieceColor.BLACK);
+            new PieceStatus(new PiecePosition('a', '2'), piece);
+        }).doesNotThrowAnyException();
+    }
+}
