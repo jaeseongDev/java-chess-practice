6c75d991c3406c72004bf87cf8bd23226db66d11
jaeseongDev <qkrwotjd1445@naver.com>
2021-03-15 17:20:19 +0900

    feat : 체스 말의 포지션 클래스에서 포지션을 검증하는 코드 추가

--- src/main/java/chess/domain/piece/PiecePosition.java
+++ src/main/java/chess/domain/piece/PiecePosition.java
@@ -1,13 +1,25 @@
 package chess.domain.piece;
 
 public class PiecePosition {
-    private final String x;
-    private final String y;
+    private static final char MIN_X_POSITION = 'a';
+    private static final char MAX_X_POSITION = 'h';
+    public static final char MIN_Y_POSITION = '1';
+    public static final char MAX_Y_POSITION = '8';
+    private final char x;
+    private final char y;
 
-    public PiecePosition(String x, String y) {
+    public PiecePosition(char x, char y) {
+        validatePosition(x, y);
         this.x = x;
         this.y = y;
     }
 
-
+    private void validatePosition(char x, char y) {
+        if (x < MIN_X_POSITION || x > MAX_X_POSITION) {
+            throw new IllegalArgumentException("가로의 범위에 해당하는 값만 넣어야 합니다.");
+        }
+        if (y < MIN_Y_POSITION || y > MAX_Y_POSITION) {
+            throw new IllegalArgumentException("세로의 범위에 해당하는 값만 넣어야 합니다.");
+        }
+    }
 }
--- src/test/java/chess/domain/piece/PiecePositionTest.java
+++ src/test/java/chess/domain/piece/PiecePositionTest.java
@@ -1,18 +1,28 @@
 package chess.domain.piece;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class PiecePositionTest {
-    @Test
-    @DisplayName("체스 말의 포지션을 정상적으로 생성")
-    public void init() {
-        String x = "a";
-        String y = "1";
+    @ParameterizedTest(name = "{displayName}")
+    @DisplayName("x가 1~8, y가 a~h에 해당하는 값일 경우에 정상적으로 인스턴스 생성하는 지 테스트")
+    @CsvSource(value = {"1:a", "2:b", "7:g", "8:h"}, delimiter = ':')
+    public void init(char x, char y) {
         assertThatCode(() -> {
             new PiecePosition(x, y);
         }).doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest(name = "{displayName}")
+    @DisplayName("x가 1~8, y가 a~h에 해당하는 값을 벗어나는 경우에 예외 발생")
+    @CsvSource(value = {"1:i", "2:j", "0:a", "9:a"}, delimiter = ':')
+    public void init_ThrowException(char x, char y) {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new PiecePosition(x, y);
+        });
     }
 }
